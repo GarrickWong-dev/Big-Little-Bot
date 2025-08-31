ae05a0bf7bc371023135b9cd07f732f62a44ea25
root <root@GarrickLaptop.localdomain>
2025-08-31 03:26:28 -0400

    deleted new rteam function

--- backend/webController.py
+++ backend/webController.py
@@ -1,17 +1,6 @@
 from . import big_little_db
 from .sub import Submission
-from . import apiKeys
-import os
 from . import discordNoti
-
-def newTeam(username, password, key):
-        if key == apiKeys.hiddenKey:
-                if big_little_db.addUser(username, password): #make sure we add user
-                        return "team added"
-                else:
-                        return "name taken"
-        else:
-                return "invalid key"
 
 def newSubmit(teamName, challenge, points, date, picBytes, picName):
         newSub = Submission (teamName, challenge, points, date, picBytes, picName)
