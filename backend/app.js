e545689659e9fdb0a768fa3ec89cc1a0197683a8
GarrickWong-dev <wongg36@mcmaster.ca>
2026-09-05 17:10:22 -0400

    enable cors and fix deployed login

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,11 @@
 const express = require('express');
 const path = require('path');
 const apiRoutes = require('./route');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
